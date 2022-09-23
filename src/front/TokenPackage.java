43f1ed1c77d043268403b13791670e1a15e7ea39
北冥哲雨 <gongyue@beimingzheyudeMacBook-Pro.local>
2022-09-23 14:53:59 +0800

    添加几个文件

--- src/front/TokenPackage.java
+++ src/front/TokenPackage.java
@@ -1,17 +1,16 @@
 package front;
 
+import java.io.LineNumberReader;
 import java.util.List;
 
 public class TokenPackage {
     private int pointer;
     private final List<Token> tokenList;
-    private Token.Type curToken;
-    private String curInfo;
+    private Token curToken;
 
     public TokenPackage(List<Token> tokenList) {
         this.pointer = 0;
         this.tokenList = tokenList;
-        this.curInfo = null;
         this.curToken = null;
     }
 
@@ -31,16 +30,26 @@ public class TokenPackage {
         if (pointer == tokenList.size()) {
             return;
         }
-        curToken = tokenList.get(pointer).type();
-        curInfo = tokenList.get(pointer).stringInfo();
+        curToken = tokenList.get(pointer);
         pointer += 1;
     }
 
-    public String getCurInfo() {
-        return curInfo;
+    public Token preview(int offset){
+        if(pointer + offset > tokenList.size()){
+            return null;
+        }
+        return tokenList.get(pointer + offset - 1);
     }
 
-    public Token.Type getCurToken() {
+    public Token getCurToken() {
         return curToken;
+    }
+
+    public Token get(int index) {
+        if (index < tokenList.size()) {
+            return tokenList.get(index);
+        } else {
+            return null;
+        }
     }
 }
---
+++ src/front/nodes/Node.java
@@ -0,0 +1,4 @@
+package front.nodes;
+
+public interface Node {
+}
